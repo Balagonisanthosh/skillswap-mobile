14a2bbdd082266795174269904e6c5b2d6da5a4a
Balagonisanthosh <balagonisanthosh445@gmail.com>
2026-03-10 03:46:26 +0530

    updated the _layout file

--- skillswap/app/_layout.tsx
+++ skillswap/app/_layout.tsx
@@ -1,20 +1,31 @@
 import { Stack } from "expo-router";
 import { useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
 import { useAuthStore } from "../store/AuthStore";
+
 export default function RootLayout() {
   const { loadToken, isAuthenticated, isLoading } = useAuthStore();
+
   useEffect(() => {
     loadToken();
   }, []);
-  if (isLoading) return null;
+
+  // Show loader while checking token
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {" "}
       {isAuthenticated ? (
         <Stack.Screen name="(drawer)" />
       ) : (
         <Stack.Screen name="(auth)" />
-      )}{" "}
+      )}
     </Stack>
   );
 }
