0fe0e6db4af3d1dc4cd2c68735296f6f8196492d
Balagonisanthosh <balagonisanthosh445@gmail.com>
2026-03-10 03:37:57 +0530

    removed the AnimationSplash file

--- skillswap/app/_layout.tsx
+++ skillswap/app/_layout.tsx
@@ -1,29 +1,20 @@
 import { Stack } from "expo-router";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuthStore } from "../store/AuthStore";
-import SplashAnimation from "./components/AnimationSplash";
-
 export default function RootLayout() {
   const { loadToken, isAuthenticated, isLoading } = useAuthStore();
-  const [showSplash, setShowSplash] = useState(true);
-
   useEffect(() => {
     loadToken();
   }, []);
-
-  if (showSplash) {
-    return <SplashAnimation onFinish={() => setShowSplash(false)} />;
-  }
-
   if (isLoading) return null;
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      {" "}
       {isAuthenticated ? (
         <Stack.Screen name="(drawer)" />
       ) : (
         <Stack.Screen name="(auth)" />
-      )}
+      )}{" "}
     </Stack>
   );
 }
