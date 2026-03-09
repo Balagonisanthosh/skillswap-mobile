d6ae16e8a1759860b6cbbbb8eba19e5e9a9c7a67
Balagonisanthosh <balagonisanthosh445@gmail.com>
2026-03-10 04:41:31 +0530

    added the refresh token

--- skillswap/app/_layout.tsx
+++ skillswap/app/_layout.tsx
@@ -1,11 +1,41 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
+import { useAuthStore } from "../store/AuthStore";
 
 export default function RootLayout() {
-  
+  const loadToken = useAuthStore((state) => state.loadToken);
+  const token = useAuthStore((state) => state.token);
+  const isLoading = useAuthStore((state) => state.isLoading);
+
+  useEffect(() => {
+    loadToken();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator size="large" color="#4A90E2" />
+      </View>
+    );
+  }
+
   return (
-    <Stack screenOptions={{ headerShown: false, animation: "slide_from_right" }}>
-      <Stack.Screen name="(auth)" />
-      <Stack.Screen name="(drawer)" />
+    <Stack screenOptions={{ headerShown: false }}>
+      {token ? (
+        <Stack.Screen name="(drawer)" />
+      ) : (
+        <Stack.Screen name="(auth)" />
+      )}
     </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  loaderContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+});
--- skillswap/services/api.ts
+++ skillswap/services/api.ts
@@ -25,3 +25,38 @@ api.interceptors.request.use(
 );
 
 
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      console.log("⚠️ Access token expired. Refreshing token...");
+
+      originalRequest._retry = true;
+
+      try {
+        const refreshResponse = await axios.post(
+          "http://10.159.208.168:3000/api/auth/refresh",
+          {},
+          { withCredentials: true }
+        );
+
+        const newAccessToken = refreshResponse.data.accessToken;
+
+        console.log("✅ New access token generated:", newAccessToken);
+
+        await SecureStore.setItemAsync("authToken", newAccessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return api(originalRequest);
+      } catch (err) {
+        console.log("❌ Refresh token expired");
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
